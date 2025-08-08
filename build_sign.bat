2109f21f291c4e2d6b12462912fc7d65d4a5a79c
fangrong <darkbluesky@outlook.com>
2025-08-08 17:49:29 +0800

    Rename folder name to universal!

--- build_sign.bat
+++ build_sign.bat
@@ -1,5 +1,5 @@
 del *.apk
 del *.idsig
-java -jar apktool.jar b -o DraStic_r2.6.0.4a_build109_armv7_arm64_x86_rev.apk armv7_arm64
-java -jar uber-apk-signer.jar --apks DraStic_r2.6.0.4a_build109_armv7_arm64_x86_rev.apk --ks drasticrev.jks --ksAlias drasticrev
+java -jar apktool.jar b -o DraStic_r2.6.0.4a_build109_universal_rev.apk universal
+java -jar uber-apk-signer.jar --apks DraStic_r2.6.0.4a_build109_universal_rev.apk --ks drasticrev.jks --ksAlias drasticrev
 pause
