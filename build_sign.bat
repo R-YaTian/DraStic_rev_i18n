1e350d1d610da8c8dedae2dda7b218025e490123
fangrong <darkbluesky@outlook.com>
2025-08-14 13:48:23 +0800

    Revert "Update build script and ignore tmp directory"
    This reverts commit 2ca7d09d4e93ef90c97bb8ea538b80cca808bb73.

--- build_sign.bat
+++ build_sign.bat
@@ -1,12 +1,5 @@
 del *.apk
 del *.idsig
-java -jar apktool.jar b -o app.apk universal
-
-md tmp && cd tmp
-unzip ../app.apk > nul
-zip -r9 ../DraStic_r2.6.0.4a_build109_universal_rev.apk ./* > nul
-cd ..
-rd /s /q tmp
-
+java -jar apktool.jar b -o DraStic_r2.6.0.4a_build109_universal_rev.apk universal
 java -jar uber-apk-signer.jar --apks DraStic_r2.6.0.4a_build109_universal_rev.apk --ks drasticrev.jks --ksAlias drasticrev
 pause
